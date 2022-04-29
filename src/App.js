bae8aa0291ccf131b13577c0d95d943d714c73a3
pro100ton <pro100ton@gmail.com>
2022-04-29 17:30:16 +0300

    Initial commit with counter project

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
-import React, {useContext} from 'react';
-
-import Login from './components/Login/Login';
-import Home from './components/Home/Home';
-import MainHeader from './components/MainHeader/MainHeader';
-import AuthContext from './store/auth-context';
+import React from 'react';
+import BackwardCounter from './components/BackwardCounter';
+import ForwardCounter from './components/ForwardCounter';
 
 function App() {
-    const ctx = useContext(AuthContext);
-
-    return (
-        <React.Fragment>
-            <MainHeader/>
-            <main>
-                {!ctx.isLoggedIn && <Login/>}
-                {ctx.isLoggedIn && <Home/>}
-            </main>
-        </React.Fragment>
-    );
+  return (
+    <React.Fragment>
+      <ForwardCounter />
+      <BackwardCounter />
+    </React.Fragment>
+  );
 }
 
 export default App;
