a5bf9bc9eb85f5a3b10bf558f2e159be99c965a2
pro100ton <pro100ton@gmail.com>
2022-04-29 17:22:37 +0300

    Post request example

--- src/App.js
+++ src/App.js
@@ -1,73 +1,83 @@
-import React, { useState, useEffect, useCallback } from 'react';
+import React, {useState, useEffect, useCallback} from 'react';
 
 import MoviesList from './components/MoviesList';
 import AddMovie from './components/AddMovie';
 import './App.css';
 
 function App() {
-  const [movies, setMovies] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
+    const [movies, setMovies] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState(null);
 
-  const fetchMoviesHandler = useCallback(async () => {
-    setIsLoading(true);
-    setError(null);
-    try {
-      const response = await fetch('https://swapi.dev/api/films/');
-      if (!response.ok) {
-        throw new Error('Something went wrong!');
-      }
+    const fetchMoviesHandler = useCallback(async () => {
+        setIsLoading(true);
+        setError(null);
+        try {
+            const response = await fetch('https://testreact-eb9bf-default-rtdb.europe-west1.firebasedatabase.app/movies.json');
+            if (!response.ok) {
+                throw new Error('Something went wrong!');
+            }
 
-      const data = await response.json();
+            const data = await response.json();
 
-      const transformedMovies = data.results.map((movieData) => {
-        return {
-          id: movieData.episode_id,
-          title: movieData.title,
-          openingText: movieData.opening_crawl,
-          releaseDate: movieData.release_date,
-        };
-      });
-      setMovies(transformedMovies);
-    } catch (error) {
-      setError(error.message);
+            const loadedMovies = [];
+            for (const key in data){
+                loadedMovies.push({
+                    id: key,
+                    title: data[key].title,
+                    releaseDate: data[key].releaseDate,
+                    openingText: data[key].openingText
+                })
+            }
+
+            setMovies(loadedMovies);
+        } catch (error) {
+            setError(error.message);
+        }
+        setIsLoading(false);
+    }, []);
+
+    useEffect(() => {
+        fetchMoviesHandler();
+    }, [fetchMoviesHandler]);
+
+    async function addMovieHandler(movie) {
+        const response = await fetch('https://testreact-eb9bf-default-rtdb.europe-west1.firebasedatabase.app/movies.json', {
+            method: 'POST',
+            body: JSON.stringify(movie),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+        const data = await response.json()
+        console.log(data)
     }
-    setIsLoading(false);
-  }, []);
 
-  useEffect(() => {
-    fetchMoviesHandler();
-  }, [fetchMoviesHandler]);
+    let content = <p>Found no movies.</p>;
 
-  function addMovieHandler(movie) {
-    console.log(movie);
-  }
+    if (movies.length > 0) {
+        content = <MoviesList movies={movies}/>;
+    }
 
-  let content = <p>Found no movies.</p>;
+    if (error) {
+        content = <p>{error}</p>;
+    }
 
-  if (movies.length > 0) {
-    content = <MoviesList movies={movies} />;
-  }
+    if (isLoading) {
+        content = <p>Loading...</p>;
+    }
 
-  if (error) {
-    content = <p>{error}</p>;
-  }
-
-  if (isLoading) {
-    content = <p>Loading...</p>;
-  }
-
-  return (
-    <React.Fragment>
-      <section>
-        <AddMovie onAddMovie={addMovieHandler} />
-      </section>
-      <section>
-        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
-      </section>
-      <section>{content}</section>
-    </React.Fragment>
-  );
+    return (
+        <React.Fragment>
+            <section>
+                <AddMovie onAddMovie={addMovieHandler}/>
+            </section>
+            <section>
+                <button onClick={fetchMoviesHandler}>Fetch Movies</button>
+            </section>
+            <section>{content}</section>
+        </React.Fragment>
+    );
 }
 
 export default App;
