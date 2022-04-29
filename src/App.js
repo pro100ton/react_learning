9697aab74b6fc09eb6bfd9acfbaaa9928e0f98f3
pro100ton <pro100ton@gmail.com>
2022-04-29 17:03:44 +0300

    Refactoring + new components

--- src/App.js
+++ src/App.js
@@ -1,66 +1,73 @@
-import React, {useCallback, useEffect, useState} from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 
 import MoviesList from './components/MoviesList';
+import AddMovie from './components/AddMovie';
 import './App.css';
 
 function App() {
-    const [movies, setMovies] = useState([]);
-    const [isLoading, setIsLoading] = useState(false)
-    const [error, setError] = useState(null)
+  const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
 
-    // We are using useCallback here to ensure, that this function would not recreate at every re-render of
-    // an component, because it is always getting constant return values
-    const fetchMoviesHandler = useCallback(async () => {
-        setIsLoading(true);
-        setError(null)
-        try {
-            const response = await fetch('https://swapi.dev/api/films');
-            if (!response.ok) {
-                throw new Error('Something went wrong')
-            }
-            const data = await response.json();
-            const transformedMovies = data.results.map((movieData) => {
-                return {
-                    id: movieData.episode_id,
-                    title: movieData.title,
-                    openingText: movieData.opening_crawl,
-                    releaseDate: movieData.release_date
-                }
-            })
-            setMovies(transformedMovies);
-        } catch (error) {
-            setError(error.message)
-        }
-        setIsLoading(false)
-    }, [])
+  const fetchMoviesHandler = useCallback(async () => {
+    setIsLoading(true);
+    setError(null);
+    try {
+      const response = await fetch('https://swapi.dev/api/films/');
+      if (!response.ok) {
+        throw new Error('Something went wrong!');
+      }
 
-    // We use function with useCallback to ensure that we will not get an infinite loop at every rerender,
-    // because snapshot of fetchMoviesHandler is saved via useCallback()
-    useEffect(() => {
-        fetchMoviesHandler();
-    }, [fetchMoviesHandler])
+      const data = await response.json();
 
-    let content = <p>Found no movies :(</p>
-    if (movies.length > 0) {
-        content = <MoviesList movies={movies}/>
+      const transformedMovies = data.results.map((movieData) => {
+        return {
+          id: movieData.episode_id,
+          title: movieData.title,
+          openingText: movieData.opening_crawl,
+          releaseDate: movieData.release_date,
+        };
+      });
+      setMovies(transformedMovies);
+    } catch (error) {
+      setError(error.message);
     }
-    if (error) {
-        content = <p>{error}</p>
-    }
-    if (isLoading) {
-        content = <p>Loading...</p>
-    }
+    setIsLoading(false);
+  }, []);
 
-    return (
-        <React.Fragment>
-            <section>
-                <button onClick={fetchMoviesHandler}>Fetch Movies</button>
-            </section>
-            <section>
-                {content}
-            </section>
-        </React.Fragment>
-    );
+  useEffect(() => {
+    fetchMoviesHandler();
+  }, [fetchMoviesHandler]);
+
+  function addMovieHandler(movie) {
+    console.log(movie);
+  }
+
+  let content = <p>Found no movies.</p>;
+
+  if (movies.length > 0) {
+    content = <MoviesList movies={movies} />;
+  }
+
+  if (error) {
+    content = <p>{error}</p>;
+  }
+
+  if (isLoading) {
+    content = <p>Loading...</p>;
+  }
+
+  return (
+    <React.Fragment>
+      <section>
+        <AddMovie onAddMovie={addMovieHandler} />
+      </section>
+      <section>
+        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
+      </section>
+      <section>{content}</section>
+    </React.Fragment>
+  );
 }
 
 export default App;
